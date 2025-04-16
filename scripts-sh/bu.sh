f1f56b6a03b6b5a301b4ababd609c97593338616
unserori <pd.ai.374005475@gmail.com>
2025-04-16 22:00:39 +0900

    [add]bu.shでymlファイルを一行ずつ読み取る処理

--- scripts-sh/bu.sh
+++ scripts-sh/bu.sh
@@ -1,2 +1,22 @@
-# TODO: ファイルを圧縮して
-# TODO: FTP送信
+#!/bin/bash
+
+source .env
+# TODO: アンコメント
+# source ../.env
+
+declare -A bu_dir
+while IFS=':' read -r key value; do # IFSで': 'とするとそれぞれのcharで区切ることになる、だから':'一文字で区切り空白を削除している
+  [[ "$key" =~ ^[[:space:]]*# ]] && continue # インデント付きコメントに対応するため、空白が0文字以上かつ#が続くものを対象とする
+  [[ -z "$key" ]] && continue
+
+  sv_world_name=$(echo "$key" | xargs) # CONTEXT: xargsで渡し先コマンドがないと、デフォルトでecho
+  dir=$(echo "$value" | xargs)
+  
+  echo "key: $sv_world_name"
+  echo "value: $dir"
+
+  # TODO: ファイルを圧縮して
+  
+  # TODO: FTP送信
+
+done < "dir_list.yml"
