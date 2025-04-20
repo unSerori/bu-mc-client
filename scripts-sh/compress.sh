fe530b7b15a24ce900f897e2427ee372e61a05db
unserori <pd.ai.374005475@gmail.com>
2025-04-20 11:05:56 +0900

    [add]圧縮処理を関数化しファイルに切り出した

---
+++ scripts-sh/compress.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# ディレクトリを圧縮する
+
+# Parameters
+# $1 o_fn_without_ext: 文字列
+#   圧縮後のファイル名で、拡張子抜き
+# $2 dir: 文字列
+#   圧縮する対象のディレクトリ
+
+compress_dir() {
+  local o_fn_without_ext="${1}"
+  local dir="${2}"
+
+  local wd="$(pwd)"
+  local o_dir="${wd}/../temp/"
+  local o_path="${o_dir}${o_fn_without_ext}"
+  local OUT_LOG_PATH="${wd}/${LOG_DIR}${OUT_LOG_FILE}"
+  local ERR_LOG_PATH="${wd}/${LOG_DIR}${ERR_LOG_FILE}"
+
+  # 圧縮率が高いと思われる方法順で圧縮を試みる
+  {  
+    echo
+    echo "date: $(date)"
+    echo "Trying compression methods in order of expected compression ratio..."
+  } >> $OUT_LOG_PATH
+  (
+    failtest7z a -mx=9 "${o_path}.7z" "${dir}" >> "$OUT_LOG_PATH" 2>> "$ERR_LOG_PATH" && \
+    echo 7z
+  ) && return 0 || ( # サブシェルなので`cd "${wd}"`で戻ってくる必要がない
+    cd "$(dirname "${dir}")" && \
+    failtesttar -czvf "${o_path}.tar.gz" "$(basename "${dir}")" >> "$OUT_LOG_PATH" 2>> "$ERR_LOG_PATH" && \
+    echo tar.gz
+  ) && return 0 || (
+    cd "$(dirname "${dir}")" && \
+    failtestzip -r -9 "${o_path}.zip" "$(basename "${dir}")" >> "$OUT_LOG_PATH" 2>> "$ERR_LOG_PATH" && \
+    echo zip
+  ) && return 0 || (
+    {
+      echo
+      echo "date: $(date)"
+      echo "Tried multiple compression methods, but all failed."
+    } >> "$ERR_LOG_PATH"
+    return 1
+  )
+}
