2b643502d1647b16ad81ff62f45a3d3aa9b2bc61
unserori <pd.ai.374005475@gmail.com>
2025-04-20 15:26:59 +0900

    [fix]カレントディレクトリからのtempディレクトリの相対パスを修正、mkdir実行時に-pオプションを追加して二度目以降のエラーを出さなくした

--- scripts-sh/put_sftp.sh
+++ scripts-sh/put_sftp.sh
@@ -19,8 +19,8 @@ compress_file_name=$4
 
 sftp -oport="$bu_sv_port" -i ../key/key -o UserKnownHostsFile=../key/known_hosts mc-user@${server_ip} <<EOF
 cd saves
-mkdir "$save_dir_name_sv_world"
+mkdir -p "$save_dir_name_sv_world"
 cd "$save_dir_name_sv_world"
-put "./temp/${compress_file_name}"
+put "../temp/${compress_file_name}"
 bye
 EOF
